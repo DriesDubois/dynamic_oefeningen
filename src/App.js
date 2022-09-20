f9950908dec67e671f091b09df5fb98cd33a9cc3
Dries <dries.dubois@gmail.com>
2022-09-20 10:17:43 +0200

    oefeningen tot 4

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         pannekoek
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Menu</h1>
+                <div>Cola</div>
+                <div>Water</div>
+                <div>Bier</div>
+                <div>Wijn</div>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
