9700ea5e4bddcbedce5b770f399ed694938f8813
Dries <dries.dubois@gmail.com>
2022-09-27 14:45:34 +0200

    bundle 3 ex 3.2

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,4 +1,5 @@
 import * as PropTypes from "prop-types";
+import {COLOR_TRANSLATION} from "../data/data";
 
 
 Cars.propTypes = {
@@ -25,17 +26,35 @@ export function Cars(props) {
     )
 }
 
+
+
 export function Car(props){
     const {car} = props;
-
+    // const {carColor} = (COLOR_TRANSLATION.filter(translation => translation.dutch === car.color).map(translated => translated.english));
+    // const {carColor} = translateColorToDutch(COLOR_TRANSLATION, car.color);
+    COLOR_TRANSLATION.find(el => el.dutch === "blauw")
+    console.log(COLOR_TRANSLATION.find(el => el.dutch === "blauw").english)
 
     return (
         <div className="d-flex flex-column space-between mb-5" style={{background:"white"}}>
         <h2 className="text-center mb-1">{car.name}</h2>
             <p className="text-center">{car.brand &&<span>merk: {car.brand}</span> }</p>
             <p className="text-center">{car.type &&<span>type: {car.type}</span> }</p>
-            <p className="text-center">{car.color &&<span>kleur: {car.color}</span> }</p>
+            <p style={{background: getBackground(COLOR_TRANSLATION,car.color)}} className="text-center">{car.color &&<span>kleur: {car.color}</span> }</p>
+            <p></p>
+
         </div>
     )
+
+}
+
+function getBackground(array , color) {
+    console.log(array.find(el => el.dutch === color).english)
+    return array.find(el => el.dutch === color).english;
+}
+
+function translateColorToDutch(translationTable,color){
+
+    return translationTable.find(el => el.dutch === color).english
 
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,9 @@
 export const PRODUCTS_DATA = [
     {name: "cola", price: 1},
-    {name: "water", price: 1, note:"plat of bruisend"},
-    {name: "bier", price: 1, size:25},
-    {name: "bier groot", price: 1, size:33},
-    {name: "wijn", price: 2, note:"rood, wit of rosé"},
+    {name: "water", price: 1, note: "plat of bruisend"},
+    {name: "bier", price: 1, size: 25},
+    {name: "bier groot", price: 1, size: 33},
+    {name: "wijn", price: 2, note: "rood, wit of rosé"},
     {name: "cava", price: 3}
 ];
 
@@ -37,6 +37,34 @@ export const PICTURES_DATA = [
 
 export const NUMBERS_DATA = [1, 3, 5, 2, 6, 7, 34, 12, 66, 98, 11, 2];
 
+export const COLOR_TRANSLATION = [
+    {
+        dutch: "blauw",
+        english: "blue"
+    },
+    {
+        dutch: "rood",
+        english: "red"
+    },
+    {
+        dutch: "geel",
+        english: "yellow"
+    },
+    {
+        dutch: "groen",
+        english: "green"
+    },
+    {
+        dutch: "zwart",
+        english: "black"
+    },
+    {
+        dutch: "grijs",
+        english: "grey"
+    },
+
+];
+
 export const CAR_DATA = [
     {
         id: 1,
@@ -51,7 +79,13 @@ export const CAR_DATA = [
         brand: "fiat",
         type: "punto",
         color: "geel",
+        note: "die gaat altijd kapot",
     },
     {id: 3, name: "die van mijn pa", brand: "volvo", color: "zwart"},
     {id: 4, name: "geitje", brand: "citroën",},
+    {id: 5, name: "auto1", brand: "Peugeot", type: "505", color: "grijs"},
+    {id: 6, name: "auto2", brand: "Fiat", type: "Panda"},
+    {id: 7, name: "auto3", brand: "Fiat", type: "Tipo", color: "groen"},
+    {id: 8, name: "auto4", brand: "Citroën", type: "C4", color: "rood"},
+    {id: 9, name: "auto5", brand: "Fiat", type: "Panda", color: "rood"}
 ];
