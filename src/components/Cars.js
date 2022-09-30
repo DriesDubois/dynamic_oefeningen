3dca25fc869c06f76aef94c76d1920dcdf846abe
Dries <dries.dubois@gmail.com>
2022-09-30 09:36:56 +0200

    adding note to cars and alignment of cards in cars

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -17,7 +17,7 @@ export function Cars(props) {
         <>
             <div style={{background: "lightBlue", paddingLeft: "10%", paddingRight: "10%"}}>
                 <h1 style={{textAlign: "center", marginBottom: "25px"}}>{title}</h1>
-                <div className="d-flex flex-column space-between mb-5">
+                <div className="d-flex space-between mb-5 flex-wrap justify-content-between" style={{gap:"50px"}}>
                     {array.map(c => <Car key={c.id} car={c}/> )}
                 </div>
             </div>
@@ -32,11 +32,12 @@ export function Car(props){
     const {car} = props;
 
     return (
-        <div className="d-flex flex-column space-between mb-5" style={{background:"white"}}>
+        <div className="d-flex flex-column space-between mb-5 justify-content-center" style={{background:"white",minWidth: "300px"}}>
         <h2 className="text-center mb-1">{car.name}</h2>
-            <p className="text-center">{car.brand &&<span>merk: {car.brand}</span> }</p>
-            <p className="text-center">{car.type &&<span>type: {car.type}</span> }</p>
-            <p style={car?.color && {background: getBackground(COLOR_TRANSLATION,car.color)}} className="text-center">{car.color &&<span>kleur: {car.color}</span> }</p>
+            {car.brand && <p className="text-center">merk: {car.brand} </p>}
+            {car.type && <p className="text-center">type: {car.type} </p>}
+            {car.note && <p className="text-center">note: {car.note} </p>}
+            <p style={car?.color && {background: getBackground(COLOR_TRANSLATION,car.color),marginInline:"50px"}} className="text-center">{car.color &&<span>kleur: {car.color}</span> }</p>
         </div>
     )
 
