bae88d62b4a00939b8e38b424c6ee54f9fc97444
Dries <dries.dubois@gmail.com>
2022-09-30 10:30:58 +0200

    alle oefening tot 5.6

--- src/pages/PersonsPage.js
+++ src/pages/PersonsPage.js
@@ -1,11 +1,18 @@
 import {Persons} from "../components/Persons";
+import {Numbers} from "../components/Numbers";
 
 
 export function PersonsPage(props) {
     const {array} = props;
+    const descendingByAge = [...array].sort((a, b) => b.age - a.age);
+    const allAgesSeperately = [...array].map(el => el.age);
+    const uniqueAgesSorted = [...new Set(array.map(person => person.age))].sort(((a, b) => b - a));
     return (
         <>
             <Persons persons={array} title={"Personen"}/>
+            <Persons persons={descendingByAge} title={"Personen gesorteerd op leeftijd"}/>
+            <Numbers numbers={allAgesSeperately} title={"alle leeftijden"}/>
+            <Numbers numbers={uniqueAgesSorted} title={"unieke leeftijden"}/>
         </>
     )
 }
