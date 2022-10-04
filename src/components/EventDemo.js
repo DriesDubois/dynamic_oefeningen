57e4a15b535856adc1700f56f98789878c367e34
Dries <dries.dubois@gmail.com>
2022-10-04 09:38:28 +0200

    prevetdefault beahviour of link in eventstate age

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -12,11 +12,16 @@ export function EventDemo(props) {
 
     return(
         <div style={{background: "lavender", padding: "0 10% 5% 10%"}} className="mt-3 rounded shadow-sm">
+            {/*<div  className="">*/}
+            {/*    <button onClick={()=> alert(text)}>click</button>*/}
+            {/*</div>*/}
             <div  className="">
-                <button onClick={()=> alert(text)}>click</button>
+                <Button variant="primary" onClick={(e)=> alert(`${text} (${e.clientX}, ${e.clientY}`)}>click</Button>
             </div>
-            <div  className="">
-                <Button variant="primary" onClick={()=> alert(text)}>click</Button>
+            <div style={{background:"salmon"}} onClick={(e)=> {e.preventDefault(); alert('link is clicked')}} >
+                <a href={"www.google.com"}>
+                    link
+                </a>
             </div>
         </div>
     )
