95451a90b648af95d61309203355b34b91604d92
Dries <dries.dubois@gmail.com>
2022-10-04 09:19:35 +0200

    eventhandler onclick for div of mycard

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -12,7 +12,10 @@ MyCard.propTypes = {
 export function MyCard(props) {
     const {children,title} = props;
     return (
-    <div style={{background: "white", paddingLeft: "3%", paddingRight: "3%",minWidth:"300px",maxWidth:"500px"}} className="mt-3 rounded shadow-sm">
+    <div style={{background: "white", paddingLeft: "3%", paddingRight: "3%",minWidth:"300px",maxWidth:"500px"}}
+         className="mt-3 rounded shadow-sm"
+         onClick={()=> alert(title)}
+    >
         <h1 style={{textAlign: "center", marginBottom: "25px"}}>{title}</h1>
         <Container>
             <Row>
