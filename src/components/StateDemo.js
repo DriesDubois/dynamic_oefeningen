8d0944e1375794978dcb78f4199d2199dfd302e9
Dries <dries.dubois@gmail.com>
2022-10-04 11:34:27 +0200

    exercise 4.1

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -4,17 +4,9 @@ import {MyButton} from "./MyButton";
 import {Counter} from "./Counter";
 import {OnOffDemo} from "./OnOffDemo";
 
-export function StateDemo(){
-    const [demoValue, setDemoValue] = useState(0);
-
+export function StateDemo() {
     return <Section title="state">
-        <div>de waarde van demoValue is {demoValue}</div>
-        <div>de 2e waarde van demoValue is {demoValue}</div>
-        <div>
-            <MyButton onClick={()=> setDemoValue(0)}>SET 0</MyButton>
-            <MyButton onClick={()=> setDemoValue(77)}>SET 77</MyButton>
-            <MyButton onClick={()=> setDemoValue(255)}>SET 255</MyButton>
-        </div>
+        <DemoValue/>
         <div>
             <Counter name="Teller 1"/>
             <Counter name="Teller 2"/>
@@ -23,4 +15,17 @@ export function StateDemo(){
             <OnOffDemo/>
         </div>
     </Section>
+}
+
+function DemoValue() {
+    const [demoValue, setDemoValue] = useState(0);
+    return <>
+        <div>de waarde van demoValue is {demoValue}</div>
+        <div>de waarde van demoValue is {demoValue}</div>
+        <div>
+            <MyButton onClick={() => setDemoValue(0)}>SET 0</MyButton>
+            <MyButton onClick={() => setDemoValue(77)}>SET 77</MyButton>
+            <MyButton onClick={() => setDemoValue(255)}>SET 255</MyButton>
+        </div>
+    </>
 }
