db2d520c06a97bdc32c65b95c5cb30494551ec31
Dries <dries.dubois@gmail.com>
2022-10-11 11:57:22 +0200

    einde bundel 4

--- src/pages/SearchPersonsPage.js
+++ src/pages/SearchPersonsPage.js
@@ -20,7 +20,4 @@ export function SearchPersonsPage(props) {
 function filterPersons(persons,searchInput){
     if(searchInput === "") return persons
     return persons.filter(p=>p.name.toLowerCase().includes(searchInput.toLowerCase()) || p.city.toLowerCase().includes(searchInput.toLowerCase()) )
-
-
-
 }
