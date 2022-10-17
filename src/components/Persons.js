98001fcd9a6249a47a294724aa4f6b109d0c8988
Dries <dries.dubois@gmail.com>
2022-10-17 11:48:45 +0200

    bundel 5 - delete knoppen voor persons

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,26 +1,29 @@
 import {Section} from "./Section";
 import {MyCard} from "./MyCard";
+import {MyButton} from "./MyButton";
 
 
 export function Persons(props) {
-    const {persons,defaultState} = props;
+    const {persons,defaultState,editable,selectedPerson} = props;
     const {title} = props;
     if (!persons) return;
     return (
         <Section title={title} defaultState={defaultState}>
-            {persons.map(p => <Person key={p.id} person={p}/>)}
+            {persons.map(p => <Person selectedPerson={selectedPerson} key={p.id} person={p} editable={true}/>)}
         </Section>
     )
 }
 
 export function Person(props) {
-    const {person} = props;
+    let {person,editable,selectedPerson} = props;
 
     return (
-  <MyCard title={person.name}>
+  <MyCard
+      onClick={()=> selectedPerson(selectedPerson=person)}
+      title={person.name}>
       {person.age && <p className="text-center">{person.age} </p>}
       {person.city && <p className="text-center">{person.city} </p>}
-
+      {editable && <MyButton>delete</MyButton>}
   </MyCard>
     )
 
--- src/pages/PersonsFromDbPage.js
+++ src/pages/PersonsFromDbPage.js
@@ -1,4 +1,4 @@
-import {collection, query, orderBy, addDoc, updateDoc} from 'firebase/firestore'
+import {collection, query, orderBy, addDoc, updateDoc,deleteDoc,doc,getFirestore} from 'firebase/firestore'
 import {firestoreDB} from "../services/firebase";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import {Persons} from "../components/Persons";
@@ -24,8 +24,10 @@ const personConverter = {
 export function PersonsFromDbPage() {
     const collectionRef = collection(firestoreDB, 'Persons').withConverter(personConverter);
     const [searchInput, setSearchInput] = useState("");
+    const [selectedPerson, setSelectedPerson] = useState(undefined);
     const queryRef = query(collectionRef, orderBy("name"));
     const [values, loading, error] = useCollectionData(queryRef);
+    const db = getFirestore();
     console.log({values, loading, error});
 
     async function addDummyPerson() {
@@ -44,13 +46,39 @@ export function PersonsFromDbPage() {
 
     }
 
+    async function decreaseAllAges(increment = -1) {
+        try {
+            const arrayOfPromises = values.map(person => updateDoc(person.ref, {age: person.age + increment}));
+            await Promise.all(arrayOfPromises);
+            console.log(`increment ages with ${increment} for all persons done`)
+        } catch{
+            console.log(`Error ages not incremented ${error}`)
+        }
+
+    }
+
+    async function deletePerson(person){
+        const docRef = doc(db, "Persons", person.id);
+
+        deleteDoc(docRef)
+            .then(() => {
+                console.log("Entire Document has been deleted successfully.")
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
+
     return (
         <div className="m-3">
             <MyButton onClick={() => addDummyPerson()}>add Dummy</MyButton>
             <MyButton onClick={() => incrementAllAges()}>increment all ages</MyButton>
+            <MyButton onClick={() => decreaseAllAges()}>decrease all ages</MyButton>
+            <MyButton onClick={() => deletePerson()}>delete</MyButton>
             <label htmlFor="search">search input: </label>
             <input id="search" value={searchInput} onChange={e => setSearchInput(e.target.value)}/>
-            <Persons title="personen uit de database" persons={filterPersons(values, searchInput)} defaultState={true}/>
+            <Persons title="personen uit de database" persons={filterPersons(values, searchInput)} selectedPerson={setSelectedPerson} defaultState={true} editable={true}/>
         </div>
     )
 }
